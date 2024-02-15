7af9355461e43e2c6dfe3562341b142255bc359d
Kajol Singh <skajol183@gmail.com>
2024-02-15 10:45:06 +0530

    added join chat entry page and created server file

---
+++ src/component/Join/Join.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Join.css";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+
+let candidate;
+
+const sendUser = () => {
+  candidate = document.getElementById("joinInput").value;
+  document.getElementById("joinInput").value = "";
+};
+
+const Join = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div className="JoinPage">
+      <div className="JoinContainer">
+        <img src={logo} alt="logo" />
+        <h1>SK CHAT</h1>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Your Name"
+          type="text"
+          id="joinInput"
+        />
+        <Link
+          onClick={(event) => (!name ? event.preventDefault() : null)}
+          to="/chat"
+        >
+          <button onClick={sendUser} className="joinbtn">
+            Get Started
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
+export { candidate };

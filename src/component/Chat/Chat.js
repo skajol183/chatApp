000e90d226edafbcc2a412e7426f3bfc2b3ce293
Kajol Singh <skajol183@gmail.com>
2024-02-15 11:12:55 +0530

    added chat page design

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import socketIo from "socket.io-client";
+import "./Chat.css";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import closeIcon from "../../images/closeIcon.png";
+import sendLogo from "../../images/send.png";
+import { candidate } from "../Join/Join";
+
+let socket;
+// const ENDPOINT = "http://localhost:5000/";
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState("");
+  const chatBoxRef = useRef(null);
+
+  // append function which will append event info to the container
+  const appendMessage = (message, position) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { content: message, position: position },
+    ]);
+  };
+
+  // handle submit btn when user type something
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const message = messageInput.trim();
+    if (message !== "") {
+      appendMessage(`You: ${message}`, "right");
+      socket.emit("send", message);
+      setMessageInput("");
+    }
+  };
+
   return (
-    <div>
-      <h1>chat page</h1>
+    <div className="chatPage">
+      <div className="chatContainer">
+        <div className="header">
+          <h2>SK CHAT</h2>
+          <a href="/">
+            <img src={closeIcon} alt="Close" />
+          </a>
+        </div>
+        <ReactScrollToBottom className="chatBox" ref={chatBoxRef}>
+          {messages.map((msg, index) => (
+            <div key={index} className={`message ${msg.position}`}>
+              {msg.content}
+            </div>
+          ))}
+        </ReactScrollToBottom>
+
+        <form
+          className="inputBox"
+          action="#"
+          id="formContainer"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            value={messageInput}
+            className="chatInput"
+            onChange={(e) => setMessageInput(e.target.value)}
+          />
+          <button type="submit" className="sendBtn">
+            <img src={sendLogo} alt="Send" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
